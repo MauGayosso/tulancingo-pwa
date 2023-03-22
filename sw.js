7f6839e954df854f640923e4d364f8dc7ef3083b
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 <17191170703@utectulancingo.edu.mx>
2023-03-21 21:05:37 -0600

    fix notifications

--- sw.js
+++ sw.js
@@ -89,25 +89,12 @@ self.addEventListener('push', function (event) {
     });
 });
 */
-function isNewNotificationSupported() {
-  if (!window.Notification || !Notification.requestPermission)
-      return false;
-  if (Notification.permission == 'granted')
-      throw new Error('You must only call this *before* calling Notification.requestPermission(), otherwise this feature detect would bug the user with an actual notification!');
-  try {
-      new Notification('');
-  } catch (e) {
-      if (e.name == 'TypeError')
-          return false;
-  }
-  return true;
-}
 
-if (window.Notification && Notification.permission == 'granted') {
-  // We would only have prompted the user for permission if new
-  // Notification was supported (see below), so assume it is supported.
-  doStuffThatUsesNewNotification();
-} else if (isNewNotificationSupported()) {
-  // new Notification is supported, so prompt the user for permission.
-  showOptInUIForNotifications();
+function enviarNotificacion() {
+  if (Notification.permission === "granted") {
+    var notification = new Notification("Título de la notificación", {
+      body: "Cuerpo de la notificación",
+    })
+  }
 }
+setInterval(enviarNotificacion, 60 * 1000);
