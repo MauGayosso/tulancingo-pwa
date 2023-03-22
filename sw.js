49f6c5807bb98feb9278fba1f593318252514e5d
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 <17191170703@utectulancingo.edu.mx>
2023-03-21 18:31:40 -0600

    first commit

--- sw.js
+++ sw.js
@@ -1,46 +1,46 @@
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/tulancingo-pwa/sw.js').then(function(registration) {
-        console.log('ServiceWorker registrado con éxito: ', registration.scope);
-      }, function(err) {
-        console.log('Error en el registro de ServiceWorker: ', err);
-      });
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('/tulancingo-pwa/sw.js').then(function (registration) {
+      console.log('ServiceWorker registrado con éxito: ', registration.scope);
+    }, function (err) {
+      console.log('Error en el registro de ServiceWorker: ', err);
     });
-  }
-  
-  // Instalar el Service Worker
-  self.addEventListener('install', function(event) {
-    console.log('ServiceWorker instalado correctamente');
-    event.waitUntil(
-      caches.open('cache-v1').then(function(cache) {
-        return cache.addAll([
-          '/tulancingo-pwa',
-          '/tulancingo-pwa/index.html',
-          '/tulancingo-pwa/css/style.css',
-        ]);
-      })
-    );
   });
-  
-  // Activar el Service Worker
-  self.addEventListener('activate', function(event) {
-    console.log('ServiceWorker activado correctamente');
-  });
-  
-  // Escuchar las solicitudes de recuperación
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-    );
-  });
+}
+
+// Instalar el Service Worker
+self.addEventListener('install', function (event) {
+  console.log('ServiceWorker instalado correctamente');
+  event.waitUntil(
+    caches.open('cache-v1').then(function (cache) {
+      return cache.addAll([
+        '/tulancingo-pwa',
+        '/tulancingo-pwa/index.html',
+        '/tulancingo-pwa/css/style.css',
+      ]);
+    })
+  );
+});
+
+// Activar el Service Worker
+self.addEventListener('activate', function (event) {
+  console.log('ServiceWorker activado correctamente');
+});
+
+// Escuchar las solicitudes de recuperación
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
 
 if (navigator.setAppBadge) {
-    console.log("The App Badging API is supported!");
+  console.log("The App Badging API is supported!");
 }
 
 // To display an empty badge
@@ -49,9 +49,43 @@ navigator.setAppBadge();
 // To display a number in the badge
 
 navigator.setAppBadge(42).then(() => {
-    console.log("The badge was added");
+  console.log("The badge was added");
 }).catch(e => {
-    console.error("Error displaying the badge", e);
+  console.error("Error displaying the badge", e);
 });
 
 // CONTROL CENTER NOTIFICATIONS
+
+self.addEventListener('push', function (event) {
+  console.log('[Service Worker] Push Received.');
+
+  var title = 'Primera notificacion';
+  var options = {
+    body: 'Prueba notificacion',
+    icon: '/tulancingo-pwa/static/icon.png',
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function (event) {
+  console.log('[Service Worker] Notification clicked.');
+
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow('https://maugayosso.github.io/tulancingo-pwa/')
+  );
+});
+
+// PUSH NOTIFICATIONS
+
+button.addEventListener("click", () => {
+  Notification.requestPermission().then(permission => {
+    if (permission === "granted") {
+      console.log("The user accepted");
+    }
+  });
+});
+
+const notification = new Notification("Hello World!");
