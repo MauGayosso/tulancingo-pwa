59670e583f3362f2d48688f6dc634f29c6874926
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 <17191170703@utectulancingo.edu.mx>
2023-03-21 18:16:54 -0600

    first commit

---
+++ sw.js
@@ -0,0 +1,57 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        console.log('ServiceWorker registrado con éxito: ', registration.scope);
+      }, function(err) {
+        console.log('Error en el registro de ServiceWorker: ', err);
+      });
+    });
+  }
+  
+  // Instalar el Service Worker
+  self.addEventListener('install', function(event) {
+    console.log('ServiceWorker instalado correctamente');
+    event.waitUntil(
+      caches.open('cache-v1').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/css/style.css',
+        ]);
+      })
+    );
+  });
+  
+  // Activar el Service Worker
+  self.addEventListener('activate', function(event) {
+    console.log('ServiceWorker activado correctamente');
+  });
+  
+  // Escuchar las solicitudes de recuperación
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+    );
+  });
+
+if (navigator.setAppBadge) {
+    console.log("The App Badging API is supported!");
+}
+
+// To display an empty badge
+navigator.setAppBadge();
+
+// To display a number in the badge
+
+navigator.setAppBadge(42).then(() => {
+    console.log("The badge was added");
+}).catch(e => {
+    console.error("Error displaying the badge", e);
+});
+
+// CONTROL CENTER NOTIFICATIONS
